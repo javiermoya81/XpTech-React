ed3f5374260106ce431574ef45df54f8321a216f
Javier Moya <javiermoya81@hotmail.com>
2021-11-02 00:27:41 -0300

    Se agrega componente ItemCount

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
 import './App.css';
 import NavBar from './components/navBar/NavBar';
 import ItemListContainer from './components/itemListContainer/ItemListContainer'
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
+
+  const onAdd = (contador)=>{
+    alert(`La cantidad comprada es ${contador}`)
+  }
+
   return (
     <div className="App">
       <NavBar/>
       <ItemListContainer greeting = "Hola, bienvenido al eCommerce de XpTech!!!!"/>
+      <ItemCount limite={1} stock={10} onAdd={onAdd} />
     </div>
   );
 }
-
 export default App;
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,32 @@
+import {useState} from 'react'
+
+function ItemCount({limite, stock, onAdd}){
+    const [contador, SetContador] = useState(limite);
+
+    const sumarUnidad = ()=>{
+        if(contador < stock) SetContador(contador+1) 
+    }
+
+    const restarUnidad = ()=>{
+        if(contador > limite) SetContador(contador-1)
+    }
+
+    const btnOnAdd = ()=>{
+        onAdd(contador);
+        SetContador(limite);
+    }
+
+    return(
+        
+        <div>
+            <div>
+                <h3 className="btn border border-dark">Placa video</h3>
+            </div>
+            <button className='btn btn-outline-success' onClick = {sumarUnidad}>+</button>
+            <lavel className="btn border border-info">{contador}</lavel>
+            <button className="btn btn-outline-danger" onClick = {restarUnidad}>-</button>
+            <button className="btn btn-outline-dark" onClick={btnOnAdd}>Agregar</button>
+        </div>
+    )
+}
+export default ItemCount
